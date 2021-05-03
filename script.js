c442c8a3b03b10210afdcef2f258f84bb319651c
Eduardo Pereira <augusto.pereira@protonmail.com>
2021-05-03 16:46:17 -0300

    creating files, background and snake for the game

---
+++ script.js
@@ -0,0 +1,23 @@
+const canvas = document.getElementById('snake');
+const context = canvas.getContext('2d');
+let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+const createBG = () => {
+    context.fillStyle = 'lightgreen';
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+const createSnake = () => {
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = 'green';
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+createBG();
+createSnake();
